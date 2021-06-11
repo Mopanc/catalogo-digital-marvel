48239c192ab584006f4164cb0d477cb445ffbc35
mopanc <jorgemopanc@icloud.com>
2021-06-11 15:33:31 -0300

    create routes

---
+++ js/main.js
@@ -0,0 +1,25 @@
+let routes = {
+  'dashboard': dashboard,
+  'personagens': personagens,
+  'carrinho': carrinho,
+  'ofertas': ofertas
+}
+
+function updateContentPage(pageName) {
+  if (!routes[pageName]) {
+    document.getElementById('root').innerHTML = notFound();
+    return;
+  }
+
+  document.getElementById('root').innerHTML = routes[pageName]();
+}
+
+let url = new URL(window.location.href);
+let route = url.searchParams.get('p');
+
+if (route) {
+  content = route;
+}
+
+updateContentPage(content);
+
